c5e18612b590e607440986acf71783421fdb4c9d
samdra9on <samdra9on@gmail.com>
2018-06-01 20:08:29 +0800

    Indent 4 spaces

--- webpack.config.js
+++ webpack.config.js
@@ -4,53 +4,53 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const port = process.env.PORT || 3000;
 
 module.exports = {
-  mode: 'development',
-  entry: './src/index.jsx',
-  output: {
-    filename: 'bundle.[hash].js',
-    publicPath: '/',
-  },
-  devtool: 'inline-source-map',
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        use: ['babel-loader'],
-      },
-      {
-        test: /\.css$/,
-        use: [
-          {
-            loader: 'style-loader',
-          },
-          {
-            loader: 'css-loader',
-            options: {
-              modules: true,
-              camelCase: true,
-              sourceMap: true,
+    mode: 'development',
+    entry: './src/index.jsx',
+    output: {
+        filename: 'bundle.[hash].js',
+        publicPath: '/',
+    },
+    devtool: 'inline-source-map',
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                use: ['babel-loader'],
             },
-          },
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: 'style-loader',
+                    },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: true,
+                            camelCase: true,
+                            sourceMap: true,
+                        },
+                    },
+                ],
+            },
         ],
-      },
+    },
+    resolve: {
+        extensions: ['.js', '.jsx'],
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new HtmlWebpackPlugin({
+            template: 'public/index.html',
+            favicon: 'public/favicon.ico',
+        }),
     ],
-  },
-  resolve: {
-    extensions: ['.js', '.jsx'],
-  },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new HtmlWebpackPlugin({
-      template: 'public/index.html',
-      favicon: 'public/favicon.ico',
-    }),
-  ],
-  devServer: {
-    host: 'localhost',
-    port,
-    historyApiFallback: true,
-    open: true,
-    hot: true,
-  },
+    devServer: {
+        host: 'localhost',
+        port,
+        historyApiFallback: true,
+        open: true,
+        hot: true,
+    },
 };
