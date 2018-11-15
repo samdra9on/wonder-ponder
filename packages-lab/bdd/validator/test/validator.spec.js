28b45f43e00629c4df4b560fd09963c8ede7e399
samdra9on <samdra9on@gmail.com>
2018-11-15 14:45:44 +0800

    Use eslint-plugin-mocha and eslint config cascading

--- packages-lab/bdd/validator/test/validator.spec.js
+++ packages-lab/bdd/validator/test/validator.spec.js
@@ -1,17 +1,17 @@
 const validator = require('../lib/validator');
 const { expect } = require('chai');
 
-describe('A Validator', () => {
-    it('will return no errors for valid numbers', () => {
-        expect(validator(3)).to.be.empty; // eslint-disable-line no-unused-expressions
+describe('A Validator', function() {
+    it('will return no errors for valid numbers', function() {
+        expect(validator(3)).to.be.empty;
     });
 
-    describe('will return error.nonpositive for not strictly positive numbers', () => {
-        it('like 0', () => {
+    describe('will return error.nonpositive for not strictly positive numbers', function() {
+        it('like 0', function() {
             expect(validator(0)).be.deep.equal(['error.nonpositive']);
         });
 
-        it('like -2', () => {
+        it('like -2', function() {
             expect(validator(-2)).be.deep.equal(['error.nonpositive']);
         });
     });
