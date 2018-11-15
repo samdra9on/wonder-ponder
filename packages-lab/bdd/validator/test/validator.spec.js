0e6b1582ec1b0b30e136168fe0bb3d4e718b28c7
samdra9on <samdra9on@gmail.com>
2018-11-15 17:07:46 +0800

    Stop testing the same thing again and again by using parameterized test

--- packages-lab/bdd/validator/test/validator.spec.js
+++ packages-lab/bdd/validator/test/validator.spec.js
@@ -1,38 +1,28 @@
 const validator = require('../lib/validator');
 const { expect } = require('chai');
 
+function expectedToIncludeErrorWhenInvalid(number, error) {
+    it(`like ${number}`, function() {
+        expect(validator(number)).to.include(error);
+    });
+}
 describe('A Validator', function() {
     it('will return no errors for valid numbers', function() {
         expect(validator(7)).to.be.empty;
     });
 
     describe('will include error.nonpositive for not strictly positive numbers:', function() {
-        it('like 0', function() {
-            expect(validator(0)).to.include('error.nonpositive');
-        });
-
-        it('like -2', function() {
-            expect(validator(-2)).to.include('error.nonpositive');
-        });
+        expectedToIncludeErrorWhenInvalid(0, 'error.nonpositive');
+        expectedToIncludeErrorWhenInvalid(-2, 'error.nonpositive');
     });
 
     describe('will include error.three for divisible by 3 numbers:', function() {
-        it('like 3', function() {
-            expect(validator(3)).to.include('error.three');
-        });
-
-        it('like 15', function() {
-            expect(validator(15)).to.include('error.three');
-        });
+        expectedToIncludeErrorWhenInvalid(3, 'error.three');
+        expectedToIncludeErrorWhenInvalid(15, 'error.three');
     });
 
     describe('will return error.five for divisible by 5 numbers:', function() {
-        it('like 5', function() {
-            expect(validator(5)).to.include('error.five');
-        });
-
-        it('like 10', function() {
-            expect(validator(15)).to.include('error.five');
-        });
+        expectedToIncludeErrorWhenInvalid(5, 'error.five');
+        expectedToIncludeErrorWhenInvalid(15, 'error.five');
     });
 });
