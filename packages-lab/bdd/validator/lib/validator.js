755f67272c644415504a66b90c3f81b3ebfd1422
samdra9on <samdra9on@gmail.com>
2018-11-20 22:26:19 +0800

    Organize setup

--- packages-lab/bdd/validator/lib/validator.js
+++ packages-lab/bdd/validator/lib/validator.js
@@ -1,14 +1,8 @@
-const nonPositiveValidationRule = require('./validator/rules/nonPositive');
-const nonDivisibleValidationRule = require('./validator/rules/nonDivisible');
-
-const validationRules = [
-    nonPositiveValidationRule,
-    nonDivisibleValidationRule(3, 'error.three'),
-    nonDivisibleValidationRule(5, 'error.five'),
-];
-module.exports = function validator(n) {
-    return validationRules.reduce((result, rule) => {
-        rule(n, result);
-        return result;
-    }, []);
+module.exports = function validatorWith(validationRules) {
+    return function validator(n) {
+        return validationRules.reduce((result, rule) => {
+            rule(n, result);
+            return result;
+        }, []);
+    };
 };
--- packages-lab/bdd/validator/test/validator.spec.js
+++ packages-lab/bdd/validator/test/validator.spec.js
@@ -1,33 +1,74 @@
-const validator = require('../lib/validator');
-const { expect } = require('chai');
+const chai = require('chai');
+const { expect } = chai;
+const validatorWith = require('../lib/validator');
+const nonPositiveValidationRule = require('../lib/validator/rules/nonPositive');
+const nonDivisibleValidationRule = require('../lib/validator/rules/nonDivisible');
 
-function expectedToIncludeErrorWhenInvalid(example) {
-    const { number, error } = example;
-    it(`like ${number}`, function() {
-        expect(validator(number)).to.include(error);
-    });
-}
-describe('A Validator', function() {
+describe('A validator', function() {
     it('will return no errors for valid numbers', function() {
+        const validator = validatorWith([
+            nonPositiveValidationRule,
+            nonDivisibleValidationRule(3, 'error.three'),
+            nonDivisibleValidationRule(5, 'error.five'),
+        ]);
         expect(validator(7)).to.be.empty;
     });
 
     describe('will include error.nonpositive for not strictly positive numbers:', function() {
-        [
-            { number: 0, error: 'error.nonpositive' },
-            { number: -2, error: 'error.nonpositive' },
-        ].forEach(expectedToIncludeErrorWhenInvalid);
+        it('like 0', function() {
+            const validator = validatorWith([
+                nonPositiveValidationRule,
+                nonDivisibleValidationRule(3, 'error.three'),
+                nonDivisibleValidationRule(5, 'error.five'),
+            ]);
+            expect(validator(0)).to.include('error.nonpositive');
+        });
+
+        it('like -2', function() {
+            const validator = validatorWith([
+                nonPositiveValidationRule,
+                nonDivisibleValidationRule(3, 'error.three'),
+                nonDivisibleValidationRule(5, 'error.five'),
+            ]);
+            expect(validator(-2)).to.include('error.nonpositive');
+        });
     });
 
     describe('will include error.three for divisible by 3 numbers:', function() {
-        [{ number: 3, error: 'error.three' }, { number: 15, error: 'error.three' }].forEach(
-            expectedToIncludeErrorWhenInvalid,
-        );
+        it('like 3', function() {
+            const validator = validatorWith([
+                nonPositiveValidationRule,
+                nonDivisibleValidationRule(3, 'error.three'),
+                nonDivisibleValidationRule(5, 'error.five'),
+            ]);
+            expect(validator(3)).to.include('error.three');
+        });
+        it('like 15', function() {
+            const validator = validatorWith([
+                nonPositiveValidationRule,
+                nonDivisibleValidationRule(3, 'error.three'),
+                nonDivisibleValidationRule(5, 'error.five'),
+            ]);
+            expect(validator(15)).to.include('error.three');
+        });
     });
 
-    describe('will return error.five for divisible by 5 numbers:', function() {
-        [{ number: 5, error: 'error.five' }, { number: 15, error: 'error.five' }].forEach(
-            expectedToIncludeErrorWhenInvalid,
-        );
+    describe('will include error.five for divisible by 5 numbers:', function() {
+        it('like 5', function() {
+            const validator = validatorWith([
+                nonPositiveValidationRule,
+                nonDivisibleValidationRule(3, 'error.three'),
+                nonDivisibleValidationRule(5, 'error.five'),
+            ]);
+            expect(validator(5)).to.include('error.five');
+        });
+        it('like 15', function() {
+            const validator = validatorWith([
+                nonPositiveValidationRule,
+                nonDivisibleValidationRule(3, 'error.three'),
+                nonDivisibleValidationRule(5, 'error.five'),
+            ]);
+            expect(validator(15)).to.include('error.five');
+        });
     });
 });
