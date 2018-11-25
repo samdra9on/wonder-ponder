b5229207f3a4ddc1d6afa9ba387e1048c623b316
samdra9on <samdra9on@gmail.com>
2018-11-25 23:08:18 +0800

    Customize the validator configuration, phase 3, implementation in the book

--- packages-lab/bdd/validator/lib/factory.js
+++ packages-lab/bdd/validator/lib/factory.js
@@ -2,17 +2,19 @@ const validatorWith = require('./validator');
 const nonPositiveValidationRule = require('./validator/rules/nonPositive');
 const nonDivisibleValidationRule = require('./validator/rules/nonDivisible');
 
+const ruleFactoryMap = {
+    nonPositive() {
+        return nonPositiveValidationRule;
+    },
+    nonDivisible(options) {
+        return nonDivisibleValidationRule(options.divisor, options.error);
+    },
+};
+function toValidationRule(ruleDescription) {
+    return ruleFactoryMap[ruleDescription.type](ruleDescription.options);
+}
 module.exports = function factory(findConfiguration) {
-    return function newValidator(configurationName) {
-        const configuration = findConfiguration(configurationName);
-        const rules = configuration.map(({ type, options }) => {
-            if (type === 'nonPositive') {
-                return nonPositiveValidationRule;
-            } else if (type === 'nonDivisible') {
-                return nonDivisibleValidationRule(options.divisor, options.error);
-            }
-            return null;
-        });
-        return validatorWith(rules);
+    return function newValidator(ruleSetName) {
+        return validatorWith(findConfiguration(ruleSetName).map(toValidationRule));
     };
 };
