0bc0a64b06c730935edabde6da61b417b47b19fd
samdra9on <samdra9on@gmail.com>
2018-11-27 16:15:48 +0800

    Change to a bit more complex app

--- packages-lab/react-eco/vdom/src/App.js
+++ packages-lab/react-eco/vdom/src/App.js
@@ -1,5 +1,13 @@
 import h from './hyperscript';
 
-const App = () => h('h1', null, 'Hello, vDOM!');
+const App = props => {
+    const { list } = props;
+    return h(
+        'div',
+        { class: 'app' },
+        h('h1', null, 'Simple vDOM'),
+        h('ul', null, ...list.map(item => h('li', null, item))),
+    );
+};
 
 export { App as default };
--- packages-lab/react-eco/vdom/src/index.js
+++ packages-lab/react-eco/vdom/src/index.js
@@ -1,5 +1,23 @@
 import App from './App';
 
-const app = App();
 const root = document.getElementById('root');
-root.appendChild(app);
+const getRandomItemFromArray = list => list[Math.round(Math.random() * (list.length - 1))];
+
+let currentApp;
+const render = state => {
+    const newApp = App(state);
+    if (currentApp) {
+        root.replaceChild(newApp, currentApp);
+    } else {
+        root.appendChild(newApp);
+    }
+    currentApp = newApp;
+};
+const state = {
+    list: ['🕺', '💃', '😀', '🙋‍', '💼', '🕶️️', '👏', '🤳', '🕵️', '👩‍🔧'],
+};
+render(state);
+setInterval(() => {
+    state.list = [...state.list, getRandomItemFromArray(state.list)];
+    render(state);
+}, 1000);
