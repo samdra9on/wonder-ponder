252a5c075d030fd2d06a2c0a027d8d33a315674d
samdra9on <samdra9on@gmail.com>
2018-12-04 13:29:55 +0800

    Start to implement the steps

---
+++ packages-lab/bdd/mycafe-cucumber/test/step_definitions/display_order_steps.js
@@ -0,0 +1,21 @@
+const { Given, When, Then } = require('cucumber');
+
+Given('that the order is empty', function() {
+    return 'pending';
+});
+
+When(/^the customer displays the order$/, function() {
+    return 'pending';
+});
+
+Then(/^no order items will be shown$/, function() {
+    return 'pending';
+});
+
+Then(/^"([^"]*)" will be shown as total price$/, function(expectedTotoalPrice) {
+    return 'pending';
+});
+
+Then(/^there will only be possible to add a beverage$/, function() {
+    return 'pending';
+});
