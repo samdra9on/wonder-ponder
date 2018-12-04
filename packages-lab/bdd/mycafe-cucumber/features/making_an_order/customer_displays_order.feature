ac1c1f2847e3642e8e82f95fe2c31f57a8e91e8c
samdra9on <samdra9on@gmail.com>
2018-12-04 18:41:07 +0800

    Implement the "Non empty order" scenario

--- packages-lab/bdd/mycafe-cucumber/features/making_an_order/customer_displays_order.feature
+++ packages-lab/bdd/mycafe-cucumber/features/making_an_order/customer_displays_order.feature
@@ -12,3 +12,18 @@ Feature: Customer displays order
         Then no order items will be shown
         And "0" will be shown as total price
         And there will only be possible to add a beverage
+
+
+    Scenario: Non empty order
+        Given that the order contains "1" "Espresso"
+        And that the order contains "2" "Mocaccino"
+        When the customer displays the order
+        Then the order will show "1" "Espresso"
+        And the order will show "2" "Mocaccino"
+        And "6.10" will be shown as total price
+        And there will be possible to "place order"
+        And there will be possible to "add beverage"
+        And there will be possible to "edit item quantity" for item "1"
+        And there will be possible to "remove item" from item "1"
+        And there will be possible to "edit item quantity" for item "2"
+        And there will be possible to "remove item" from item "2"
--- packages-lab/bdd/mycafe-cucumber/test/step_definitions/display_order_steps.js
+++ packages-lab/bdd/mycafe-cucumber/test/step_definitions/display_order_steps.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 const { Given, When, Then } = require('cucumber');
 const chai = require('chai');
 const orders = require('../support/examples/orders');
@@ -32,4 +33,63 @@ Then(/^there will only be possible to add a beverage$/, function() {
     return expect(this.result)
         .to.eventually.have.property('actions')
         .that.is.deep.equal([orders.actionsFor(this.order).appendItem()]);
+});
+
+Given('that the order contains {string} {string}', function(strQuantity, name) {
+    const quantity = Number(strQuantity);
+    this.orderItems = _.concat(this.orderItems || [], {
+        beverage: _.toLower(name),
+        quantity,
+    });
+    this.order = this.orderStorage.alreadyContains(orders.withItems(this.orderItems));
+
+    this.messages = this.messageStorage.alreadyContains({
+        id: this.order.id,
+        data: [],
+    });
+    this.messageStorage.updateWillNotFail();
+});
+
+Then('the order will show {string} {string}', function(strQuantity, name) {
+    const quantity = Number(strQuantity);
+    return expect(this.result)
+        .to.eventually.have.property('items')
+        .that.is.deep.include(
+            ...orders.withItems([
+                {
+                    beverage: _.toLower(name),
+                    quantity,
+                },
+            ]).data,
+        );
+});
+
+Then('there will be possible to {string}', function(legibleAction) {
+    if (legibleAction === 'place order') {
+        return expect(this.result)
+            .to.eventually.have.property('actions')
+            .that.is.deep.include(orders.actionsFor(this.order).place());
+    } else if (legibleAction === 'add beverage') {
+        return expect(this.result)
+            .to.eventually.have.property('actions')
+            .that.is.deep.include(orders.actionsFor(this.order).appendItem());
+    }
+});
+
+Then('there will be possible to {string} for item {string}', function(legibleAction, strIndex) {
+    const index = Number(strIndex) - 1;
+    if (legibleAction === 'edit item quantity') {
+        return expect(this.result)
+            .to.eventually.have.property('actions')
+            .that.is.deep.include(orders.actionsFor(this.order).editItemQuantity(index));
+    }
+});
+
+Then('there will be possible to {string} from item {string}', function(legibleAction, strIndex) {
+    const index = Number(strIndex) - 1;
+    if (legibleAction === 'remove item') {
+        return expect(this.result)
+            .to.eventually.have.property('actions')
+            .that.is.deep.include(orders.actionsFor(this.order).removeItem(index));
+    }
 });
